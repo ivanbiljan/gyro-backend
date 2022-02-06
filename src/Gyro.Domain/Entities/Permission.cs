995c60bc3c8bf6b01eafe69ac228da342e02c9f0
ivanbiljan <ssbiljan@gmail.com>
2022-02-06 01:48:55 +0100

    Add Permission entity

---
+++ src/Gyro.Domain/Entities/Permission.cs
@@ -0,0 +1,11 @@
+﻿using Gyro.Domain.Shared;
+
+namespace Gyro.Domain.Entities
+{
+    public sealed class Permission : EntityBase
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
