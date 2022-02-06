a174214bb73045dbc1f78e15d97ea8bad48d9517
ivanbiljan <ssbiljan@gmail.com>
2022-02-06 01:18:21 +0100

    Add Role entity

---
+++ src/Gyro.Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Gyro.Domain.Shared;
+
+namespace Gyro.Domain.Entities
+{
+    public sealed class Role : AuditableEntityBase
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
