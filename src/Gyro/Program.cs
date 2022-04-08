d088305c147361291ea7dca4f5f5729984324ef4
ivanbiljan <ssbiljan@gmail.com>
2022-04-08 20:44:41 +0200

    Move ConfigureSwaggerOptions to Gyro.Configuration

--- src/Gyro/Program.cs
+++ src/Gyro/Program.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Gyro.Configuration;
 using Gyro.Core.Extensions;
 using Gyro.Core.Shared.MediatrPipelineBehaviours;
 using Gyro.Infrastructure.Extensions;
@@ -139,50 +140,6 @@ namespace Gyro
                 endpoints.MapGet("/", context => context.Response.WriteAsync("OK"));
                 endpoints.MapControllers();
             });
-        }
-        
-        public class ConfigureSwaggerOptions
-            : IConfigureNamedOptions<SwaggerGenOptions>
-        {
-            private readonly IApiVersionDescriptionProvider _provider;
-
-            public ConfigureSwaggerOptions(
-                IApiVersionDescriptionProvider provider)
-            {
-                _provider = provider;
-            }
-
-            public void Configure(SwaggerGenOptions options)
-            {
-                foreach (var description in _provider.ApiVersionDescriptions)
-                {
-                    options.SwaggerDoc(
-                        description.GroupName, 
-                        CreateVersionInfo(description));
-                }
-            }
-
-            public void Configure(string name, SwaggerGenOptions options)
-            {
-                Configure(options);
-            }
-
-            private static OpenApiInfo CreateVersionInfo(
-                ApiVersionDescription description)
-            {
-                var info = new OpenApiInfo
-                {
-                    Title = "Gyro API",
-                    Version = description.ApiVersion.ToString()
-                };
-
-                if (description.IsDeprecated)
-                {
-                    info.Description += " This API version has been deprecated.";
-                }
-
-                return info;
-            }
         }
     }
 }
