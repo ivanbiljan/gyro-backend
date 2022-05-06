6ec558d760fb0ace8a7c5d8346c0e6c696002889
Ivan Biljan <ssbiljan@gmail.com>
2022-05-06 13:06:39 +0200

    Define IMayHaveTenant and IMustHaveTenant

---
+++ src/Gyro.Core/Entities/IMustHaveTenant.cs
@@ -0,0 +1,13 @@
+﻿namespace Gyro.Core.Entities;
+
+/// <summary>
+///     Represents a contract that describes an entity that must have a tenant associated with it. These entities are
+///     always filter by their tenant ID.
+/// </summary>
+public interface IMustHaveTenant
+{
+    /// <summary>
+    ///     Gets an integer that uniquely identifies an entity's tenant.
+    /// </summary>
+    int TenantId { get; }
+}
