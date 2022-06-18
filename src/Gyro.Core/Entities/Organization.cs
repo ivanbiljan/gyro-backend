a5fe9dd0a67be867b2a04d572fd2ba8ba3e7fe74
ivanbiljan <ssbiljan@gmail.com>
2022-06-18 13:31:09 +0200

    Add Organization

---
+++ src/Gyro.Core/Entities/Organization.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Gyro.Core.Shared;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Gyro.Core.Entities;
+
+/// <summary>
+/// Represents an organization.
+/// </summary>
+public sealed class Organization : AuditableEntityBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Organization"/> with the specified organization name.
+    /// </summary>
+    /// <param name="name">The name, which must not be <see langword="null"/>.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="name"/> was <see langword="null"/>.</exception>
+    public Organization(string name)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+    
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Gets or sets an in-depth explanation of what this organization does.
+    /// </summary>
+    public string? Description { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the company associated with this organization.
+    /// </summary>
+    public string? Company { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the organization's email.
+    /// </summary>
+    public string? Email { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the organization's location.
+    /// </summary>
+    public string? Location { get; set; }
+
+    /// <summary>
+    /// Gets the organization's members.
+    /// </summary>
+    public List<User> Members { get; init; } = new();
+    
+    /// <summary>
+    /// Gets the organization's projects.
+    /// </summary>
+    public List<Project> Projects { get; init; } = new();
+}
